b94f91b49816bc95fe41e659f8c12ac95ba35fed
sathwik-1309 <sathwik1309@gmail.com>
2023-09-19 17:59:17 +0530

    nav links

--- frontend/src/components/Scorebox/Scorebox.js
+++ frontend/src/components/Scorebox/Scorebox.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Scorebox.css'
 import '../css/teams.css'
 import Photocard from "../Photocard/Photocard";
+import { FRONTEND_API_URL } from "../../my_constants";
 
 function Scorebox(props) {
     const p = props.data
@@ -9,11 +10,11 @@ function Scorebox(props) {
     let balls = p.batted ? p.balls : ''
     let extra_details = <></>
     if (props.detailed) {
-        extra_details = <div className={`scorebox_footer h-30 ${p.color}1 flex-row default-font font-0_8 font-600`}>
+        extra_details = <a className={`scorebox_footer h-30 ${p.color}1 flex-row default-font font-0_8 font-600`} href={`${FRONTEND_API_URL}/match/${p.match_id}/1/summary`}>
             <div className='scorebox_pos w-110 flex vert-align' >no. {p.position}</div>
             <div className='scorebox_venue w-200 flex vert-align'>at {p.venue}</div>
             <div className='scorebox_vs_team w-100 flex vert-align'>vs {p.vs_team}</div>
-        </div>
+        </a>
     }
     // let picbox = (props.pic === false) ? <></> : <Photocard p_id = {p.p_id} height={'85px'}/>
     return (

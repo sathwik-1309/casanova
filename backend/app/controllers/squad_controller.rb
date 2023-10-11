a9d19621989aa54592e5f4722eede50235ae6a13
sathwik-1309 <sathwik1309@gmail.com>
2023-10-11 13:47:53 +0530

    squad-page

--- backend/app/controllers/squad_controller.rb
+++ backend/app/controllers/squad_controller.rb
@@ -8,14 +8,23 @@ class SquadController < ApplicationController
       "id" => squad.id,
       "color" => squad.abbrevation,
       "abbrevation" => squad.get_abb,
-      "teamname" => squad.get_teamname
+      "teamname" => squad.get_teamname,
+      "tour" => squad.tournament.get_tour_with_season
     }
-    info['captain'] = squad.captain.attributes.slice('id', 'name', 'fullname')
     hash['schedule'] = squad.schedule
     hash['bat_stats'] = squad.bat_stats
     hash['ball_stats'] = squad.ball_stats
     hash['top_players'] = squad.top_players
-    hash['info'] = info
+    captain = squad.captain.attributes.slice('id', 'name', 'fullname')
+    captain['fullname'] = captain['fullname'].titleize
+    hash['top_players']['captain'] = captain
+    hash['top_players'].each do|key, value|
+      if value['player']
+        value['player']['fullname'] = value['player']['fullname'].titleize
+      end
+    end
+
+    hash['meta'] = meta
     render(:json => Oj.dump(hash))
   end
 
--- frontend/src/pages/Squad/SquadPage/SquadPage.js
+++ frontend/src/pages/Squad/SquadPage/SquadPage.js
@@ -5,6 +5,7 @@ import { BACKEND_API_URL } from '../../../my_constants';
 import ScheduleMatchBox from '../../../components/Tournament/Schedule/ScheduleMatchBox/ScheduleMatchBox';
 import './SquadPage.css'
 import ScheduleMatchBox2 from '../../../components/Tournament/Schedule/ScheduleMatchBox2/ScheduleMatchBox2';
+import Photocard from '../../../components/Player/Photocard/Photocard';
 
 function SquadSchedules(props) {
   const data = props.data
@@ -16,6 +17,63 @@ function SquadSchedules(props) {
         return(<ScheduleMatchBox2 data={schedule}/>)
       })
     }
+    </div>
+  )
+}
+
+function SquadHeader(props) {
+  return(
+    <div className={`flex-col font-1_2 font-600 w-400 flex-centered bg-shadow squad-header ${props.data.color}1`}>
+        <div className='squad-header-line1 flex-centered'>{props.data.teamname}</div>
+        <div className='font-400 font-0_8 squad-header-line2'>{props.data.tour}</div>
+      </div>
+  )
+}
+
+function CaptainBox(props) {
+  const data = props.data
+  return (
+      <div className={`flex-col ${props.meta.color}1 captain-box`}>
+        <div className={`font-600 font-0_7 flex-centered pt-3 ${props.meta.color}2`}>CAPTAIN</div>
+        <div className='flex-row'>
+          <Photocard p_id={data.id} color={props.meta.color} height='60px'/>
+          <div className='flex-centered font-1 font-600 flex-grow w-180 pr-30'> {data.fullname} </div>
+        </div>
+      </div>
+  )
+}
+
+function PerformerBox({header, player, info, meta, label}) {
+  return (
+    <div className='flex-col performer-box'>
+      <div className={`font-600 font-0_7 flex-centered pt-3 ${meta.color}2`}>{header}</div>
+      <div className={`flex-row ${meta.color}1 font-600 font-1 flex-centered`}>
+        <Photocard p_id={player.id} color={meta.color} height='60px' />
+        <div className='w-180 lp-10'>{player.fullname}</div>
+        <div className='w-100 flex-centered'>{info} <span className='font-500 font-0_7 pt-3 pl-5'> {label}</span></div>
+      </div>
+    </div>
+  )
+}
+
+function SquadPerformers(props) {
+  const data = props.data
+  console.log(data)
+  return (
+    <div className='flex-col bg-white bg-shadow squad-performers flex-centered'>
+      <CaptainBox data={data.captain} meta={props.meta}/>
+      <div className='flex-row squad-performers-rows'>
+        <PerformerBox header='MOST RUNS' meta={props.meta} player={data.most_runs.player} info={data.most_runs.runs} label='RUNS'/>
+        <PerformerBox header='MOST WICKETS' meta={props.meta} player={data.most_wickets.player} info={data.most_wickets.wickets} label='WICKETS'/>
+      </div>
+      <div className='flex-row squad-performers-rows'>
+        <PerformerBox header='BEST STRIKE-RATE' meta={props.meta} player={data.best_sr.player} info={data.best_sr.sr} label='SR'/>
+        <PerformerBox header='BEST ECONOMY' meta={props.meta} player={data.best_economy.player} info={data.best_economy.economy} label='RPO'/>
+      </div>
+      <div className='flex-row squad-performers-rows'>
+        <PerformerBox header='BEST SCORE' meta={props.meta} player={data.best_score.player} info={data.best_score.best.score} label={data.best_score.best.balls}/>
+        <PerformerBox header='BEST SPELL' meta={props.meta} player={data.best_spell.player} info={data.best_spell.best.fig} label={data.best_spell.best.overs}/>
+      </div>
     </div>
   )
 }
@@ -40,8 +98,13 @@ function SquadPage(props) {
     }
     console.log(data)
   return (
-    <div className='squad-page'>
-      <SquadSchedules data={data.schedule} />
+    <div className='squad-page flex-col default-font'>
+      <SquadHeader data={data.meta}/>
+      <div className='flex-row squad-page-row2'>
+        <SquadSchedules data={data.schedule} />
+        <SquadPerformers data={data.top_players} meta={data.meta}/>
+      </div>
+      
     </div>
   )
 }

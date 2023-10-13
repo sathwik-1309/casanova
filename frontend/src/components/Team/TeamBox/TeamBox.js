930aea68815deb631c334f472c1967c0394c28fc
sathwik-1309 <sathwik1309@gmail.com>
2023-10-13 19:22:22 +0530

    UI modified

--- frontend/src/components/Team/TeamBox/TeamBox.js
+++ frontend/src/components/Team/TeamBox/TeamBox.js
@@ -4,12 +4,14 @@ import ProgressBar from "../../ProgressBar/ProgressBar";
 import {team_colors} from "../../Match/Graph/team_colors";
 import { Link } from 'react-router-dom';
 import { FRONTEND_API_URL } from '../../../my_constants';
+import TeamWinPercentage from '../TeamWinPercentage/TeamWinPercentage';
 
 function TeamBox(props) {
     let data = props.data
-    console.log(data)
-    const analysis1 = team_colors.analysis1
-    const analysis2 = team_colors.analysis2
+    const meta = {
+        color: data.color
+    }
+    data.total_matches = data.played
     return (
         <Link className= 'teambox_parent' to={`${FRONTEND_API_URL}/teams/${data.id}`}>
             <div className={`teambox`}>
@@ -21,14 +23,15 @@ function TeamBox(props) {
                     <div className={`teambox_tournaments ${data.color}1`}>Players: {data.players}</div>
                 }
                 
-                <div className={`teambox_tournaments ${data.color}1`}>Won: {data.won} / {data.played}</div>
+                {/* <div className={`teambox_tournaments ${data.color}1`}>Won: {data.won} / {data.played}</div> */}
                 {/*<div className={`teambox_tournaments`}>Win_p: {data.win_p} %</div>*/}
-                <div className='flex-col'>
-                    <div className='h-30 flex-centered font-500 font-0_9'>WIN %</div>
-                    <div className='flex-centered progress-container'>
+                {/* <div className='flex-col'> */}
+                    {/* <div className='h-30 flex-centered font-500 font-0_9'>WIN %</div> */}
+                    {/* <div className='flex-centered progress-container'>
                         <ProgressBar progress={data.win_p} label={`${data.win_p} %`} color1={`${analysis1}`} color2={`${analysis2}`} width='200' height='30'/>
-                    </div>
-                </div>
+                    </div> */}
+                    <TeamWinPercentage meta={meta} data={data} width='w-240' bg='bg-white'/>
+                {/* </div> */}
             </div>
         </Link>
     );
--- frontend/src/components/Team/TeamWinPercentage/TeamWinPercentage.js
+++ frontend/src/components/Team/TeamWinPercentage/TeamWinPercentage.js
@@ -4,23 +4,29 @@ import './TeamWinPercentage.css'
 function TeamWinPercentage(props) {
   const color = props.meta.color
   const data = props.data
+  const width = props.width ? props.width : 'w-300'
+  const bg = props.bg ? props.bg : 'bg-dark c-white'
   return (
     <div className='flex-col bg-white team-win-p-box bg-shadow'>
-      <div className="bg-dark c-white flex-centered h-30 font-0_9 font-600">WIN %</div>
+      <div className={`${bg} flex-centered h-30 font-0_9 font-600`}>WIN %</div>
       <div className='lp-10 pr-10'>
-        <div className='flex-row w-300 h-30 win-p-bar'>
+        <div className={`flex-row ${width} h-30 win-p-bar`}>
           <div className={`${color}1`} style={{
             width: `${data.win_p}%`
           }}></div>
           <div className={`${color}1`} style={{
-            width: `${data.loss_p}%`,
+            width: `${100-data.win_p}%`,
             opacity: '0.5'
           }}></div>
         </div>
       </div>
-      <div className='flex-row h-30 font-0_8 font-600 flex-centered'>
+      {
+        !props.limited &&
+        <div className='flex-row h-30 font-0_8 font-600 flex-centered'>
         <div className='h-30'>WON - {data.won} / {data.total_matches} <span className='font-1 font-600 lp-10'>{data.win_p}%</span></div>
       </div>
+      }
+      
       
 
     </div>
